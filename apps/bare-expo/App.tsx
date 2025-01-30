856fc5029ec1b823b022faacbb0834a5f770a999
Kudo Chien <kudo@expo.dev>
2025-01-30 09:34:45 -0800

    POC of loading specific update

--- apps/bare-expo/App.tsx
+++ apps/bare-expo/App.tsx
@@ -1,72 +1,54 @@
-import { ThemeProvider } from 'ThemeProvider';
-import * as Splashscreen from 'expo-splash-screen';
-import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import * as Updates from 'expo-updates';
+import { Button, StyleSheet, ScrollView, Text, View } from 'react-native';
 
-import MainNavigator, { optionalRequire } from './MainNavigator';
+export default function App() {
+  const updatesInfo = {
+    createdAt: Updates.createdAt,
+    updateId: Updates.updateId,
+    isEmbeddedLaunch: Updates.isEmbeddedLaunch,
+    manifest: Updates.manifest,
+  };
+  const overrideUrl =
+    process.env.EXPO_OS === 'android'
+      ? 'https://u.expo.dev/76d499c7-0aaa-4cc8-beed-05c519caca87/group/0136eb85-e4e7-4474-98aa-81c3084114d8'
+      : 'https://u.expo.dev/60b87e93-ab55-4034-aefb-c67d4f206e4d/group/0136eb85-e4e7-4474-98aa-81c3084114d8';
 
-let Notifications;
-try {
-  Notifications = require('expo-notifications');
-} catch {
-  // do nothing
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Text>{JSON.stringify(updatesInfo, null, 2)}</Text>
+        <Button
+          title="Set update overrides"
+          onPress={() => {
+            Updates.setUpdateURLAndRequestHeadersOverride({
+              updateUrl: overrideUrl,
+              requestHeaders: {
+                // 'expo-channel-name': 'preview',
+              },
+            });
+          }}
+        />
+        {/* <Button
+          title="Reset update overrides"
+          onPress={() => {
+            Updates.setUpdateURLAndRequestHeadersOverride(null);
+          }}
+        /> */}
+      </ScrollView>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-Splashscreen.setOptions({ fade: true, duration: 800 });
-
-// Require the `BackgroundTaskScreen` component from `native-component-list` if it's available
-// so that we load the module and register its background task on startup.
-optionalRequire(() => require('native-component-list/src/screens/BackgroundTaskScreen'));
-
-// Require the `BackgroundFetchScreen` component from `native-component-list` if it's available
-// so that we load the module and register its background task on startup.
-optionalRequire(() => require('native-component-list/src/screens/BackgroundFetchScreen'));
-
-const loadAssetsAsync =
-  optionalRequire(() => require('native-component-list/src/utilities/loadAssetsAsync')) ??
-  (async () => null);
-
-function useLoaded() {
-  const [isLoaded, setLoaded] = React.useState(false);
-  React.useEffect(() => {
-    let isMounted = true;
-    // @ts-ignore
-    loadAssetsAsync()
-      .then(() => {
-        if (isMounted) setLoaded(true);
-        Splashscreen.hide();
-      })
-      .catch((e) => {
-        console.warn('Error loading assets: ' + e.message);
-        if (isMounted) setLoaded(true);
-      });
-    return () => {
-      isMounted = false;
-    };
-  }, []);
-  return isLoaded;
-}
-
-export default function Main() {
-  React.useEffect(() => {
-    try {
-      const subscription = Notifications.addNotificationResponseReceivedListener(
-        ({ notification, actionIdentifier }) => {
-          console.info(
-            `User interacted with a notification (action = ${actionIdentifier}): ${JSON.stringify(
-              notification,
-              null,
-              2
-            )}`
-          );
-        }
-      );
-      return () => subscription?.remove();
-    } catch (e) {
-      console.debug('Could not have added a listener for received notification responses.', e);
-    }
-  }, []);
-
-  const isLoaded = useLoaded();
-
-  return <ThemeProvider>{isLoaded ? <MainNavigator /> : null}</ThemeProvider>;
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 64,
+    marginBottom: 16,
+    marginHorizontal: 16,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
